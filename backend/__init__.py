612af6ba5455134cbbcfdb58b228f6a1f9a3dbf4
Joseph Zhao <josephzhao15@gmail.com>
2021-04-18 12:16:27 -0700

    add cors

--- backend/__init__.py
+++ backend/__init__.py
@@ -4,9 +4,11 @@ from flask_jwt_extended import JWTManager
 from flask_sqlalchemy import SQLAlchemy
 import pymysql
 import secrets
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app)
 conn = "mysql+pymysql://{0}:{1}@{2}/{3}".format(secrets.dbuser, secrets.dbpass, secrets.dbhost, secrets.dbname)
 app.config['SQLALCHEMY_DATABASE_URI'] = conn
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
