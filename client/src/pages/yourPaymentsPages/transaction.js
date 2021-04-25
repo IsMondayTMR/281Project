622494d6dac7013c647b270d1acdf1a5b9cba911
IsMondayTMR <47375192+IsMondayTMR@users.noreply.github.com>
2021-04-24 18:06:58 -0700

    initial transaction history page

--- client/src/pages/yourPaymentsPages/transaction.js
+++ client/src/pages/yourPaymentsPages/transaction.js
@@ -1,53 +1,49 @@
 import React from 'react'
 import {Accordion, Card, Button} from 'react-bootstrap'
 function Transaction() {
-    const historyContainer = {
-        height : '100%',
-        backgroundColor : '#f0f0f0',
-        display : 'flex'
-    }
-    const HistoryContainer = {
-        width : "60%",
-        backgroundColor : 'white',
-        padding : "2.5%",
-        margin : "50px auto",
-        boxShadow: "5px 5px 15px 5px  #cfcfcf",
-    }
 
-    const title = {
-        fontSize : "2.5rem",
-        fontWeight : "bold",
-
-    }
     return (
-        <div style = {historyContainer}>
+        <>
             
-            <div style = {HistoryContainer}>
-                <p style = {title}>User History</p>
-                <Accordion>
-                    <Card>
-                        <Card.Header>
-                        <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                            History 1
-                        </Accordion.Toggle>
-                        </Card.Header>
-                        <Accordion.Collapse eventKey="0">
-                        <Card.Body>Hello! I'm the body</Card.Body>
-                        </Accordion.Collapse>
-                    </Card>
-                    <Card>
-                        <Card.Header>
-                        <Accordion.Toggle as={Button} variant="link" eventKey="1">
-                            History 2
-                        </Accordion.Toggle>
-                        </Card.Header>
-                        <Accordion.Collapse eventKey="1">
-                        <Card.Body>Hello! I'm another body</Card.Body>
-                        </Accordion.Collapse>
-                    </Card>
-                </Accordion>
+        <div>
+            <div style = {{display : "flex"} }> 
+                    <p style = {{ fontSize : "1.2rem" , fontWeight : "bold"}}>Your Transations History</p>
+        
             </div>
+            
+            <Accordion>
+            <Card style = {{border : "1px solid #bfbfbf", borderRadius : "3px", marginBottom: "5px"}}>
+              <div style = {{ width : "100%", borderRadius : "3px", backgroundColor : "#f2f2f2"}}>
+                <Accordion.Toggle as={Button}  style = {{height : "40px", 
+                                                         width : "100%", 
+                                                         textAlign: "left", 
+                                                         display : "flex", 
+                                                         justifyItems : "center",
+                                                         fontSize : "0.8rem",
+                                                         paddingTop : "9px"}} variant = "black" eventKey= "0">
+                    <div style = {{width: "475px"}}>
+                        <p > completed on 4/12/2020 </p> 
+                    </div>
+                    
+                
+                </Accordion.Toggle>
+                </div>
+                <Accordion.Collapse eventKey = "0" >
+                <div style = {{display : "flex", justifyContent : "space-between", margin: "20px 10px"}}>
+                    <div style = {{display : "flex"}}> 
+                        <p style = {{margin : "0 0", fontWeight : "bold"}}>history detail </p>
+                        <p style = {{margin : "0 10px"}}> </p>
+                    </div>
+                </div>
+                
+                </Accordion.Collapse>
+            </Card>
+            </Accordion> 
+
         </div>
+
+       
+        </>
     )
 }
 
